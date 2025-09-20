f1aafa09f246b96f685ee35f9f8845e0d537a127
RafidNabil <67823115+RafidNabil@users.noreply.github.com>
2025-09-20 20:32:02 +0600

    Update profile.jsx

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -179,7 +179,7 @@ function Profile() {
                                         startDecorator={<Delete />}
                                         variant="solid"
                                         color="danger"
-                                        onClick={() => {}}
+                                        onClick={() => handleDeletePost(post._id)}
                                         sx={{ flex: 1 }}
                                     >
                                         Delete
